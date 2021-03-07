b0a681f852150a7d8bfb79b680c38245ec2ec8ab
sogo.ohta <sogo.ohta@eure.jp>
2021-03-07 12:26:12 +0900

    Enable styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import styled from "styled-components/macro";
 import logo from "./logo.svg";
 import "./App.css";
 
+const Message = styled.div`
+  font-size: 2rem;
+  color: red;
+`;
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>
+        <p
+          css={`
+            color: purple;
+          `}
+        >
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
 
+        <Message>this is message</Message>
         <a
           className="App-link"
           href="https://reactjs.org"
